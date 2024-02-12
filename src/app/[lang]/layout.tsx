3cf8ad32838b08f41dc3eed74824e825283a3771
Diko <diko.dev99@gmail.com>
2024-02-13 00:43:48 +0700

    feat: init meta tag

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,5 @@
 import { Providers } from './(home)/providers'
 import dynamic from 'next/dynamic'
-import Head from 'next/head'
 
 import { Locale } from '@@/i18n-config'
 import { getDictionary } from '@@/get-dictionary'
@@ -19,11 +18,6 @@ export default async function RootLayout({
 
   return (
     <html lang={lang} suppressHydrationWarning>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-
       <body className="dark:bg-gray-800 min-h-screen relative">
         <Providers>
           <Header content={t.profile.navbar} lang={lang} />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import './globals.css'
 
 export default async function RootLayout({
@@ -7,36 +8,38 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <title>Hi Everyone — I am Diko Mahendra</title>
-      <meta name="title" content="Hi Everyone — I am Diko Mahendra" />
-      <meta
-        name="description"
-        content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
-      />
+        <title>Hi Everyone — I am Diko Mahendra</title>
+        <meta name="title" content="Hi Everyone — I am Diko Mahendra" />
+        <meta
+          name="description"
+          content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
+        />
 
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://cocome.vercel.app/" />
-      <meta property="og:title" content="Hi Everyone — I am Diko Mahendra" />
-      <meta
-        property="og:description"
-        content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
-      />
-      <meta property="og:image" content="/og-cover.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cocome.vercel.app/" />
+        <meta property="og:title" content="Hi Everyone — I am Diko Mahendra" />
+        <meta
+          property="og:description"
+          content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
+        />
+        <meta property="og:image" content="/og-cover.png" />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://cocome.vercel.app/" />
-      <meta
-        property="twitter:title"
-        content="Hi Everyone — I am Diko Mahendra"
-      />
-      <meta
-        property="twitter:description"
-        content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
-      />
-      <meta property="twitter:image" content="/og-cover.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://cocome.vercel.app/" />
+        <meta
+          property="twitter:title"
+          content="Hi Everyone — I am Diko Mahendra"
+        />
+        <meta
+          property="twitter:description"
+          content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
+        />
+        <meta property="twitter:image" content="/og-cover.png" />
+      </Head>
 
       <body className="dark:bg-gray-800 min-h-screen relative">{children}</body>
     </html>
