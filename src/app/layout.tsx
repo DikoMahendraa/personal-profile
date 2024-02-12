1670174756890670b45fccebec88af1bba93664e
Diko <diko.dev99@gmail.com>
2024-02-13 00:46:06 +0700

    fix: meta tag og not appear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import './globals.css'
 
 export default async function RootLayout({
@@ -8,38 +7,36 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>Hi Everyone — I am Diko Mahendra</title>
-        <meta name="title" content="Hi Everyone — I am Diko Mahendra" />
-        <meta
-          name="description"
-          content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
-        />
+      <title>Hi Everyone — I am Diko Mahendra</title>
+      <meta name="title" content="Hi Everyone — I am Diko Mahendra" />
+      <meta
+        name="description"
+        content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
+      />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://cocome.vercel.app/" />
-        <meta property="og:title" content="Hi Everyone — I am Diko Mahendra" />
-        <meta
-          property="og:description"
-          content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
-        />
-        <meta property="og:image" content="/og-cover.png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://cocome.vercel.app/" />
+      <meta property="og:title" content="Hi Everyone — I am Diko Mahendra" />
+      <meta
+        property="og:description"
+        content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
+      />
+      <meta property="og:image" content="/og-cover.png" />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://cocome.vercel.app/" />
-        <meta
-          property="twitter:title"
-          content="Hi Everyone — I am Diko Mahendra"
-        />
-        <meta
-          property="twitter:description"
-          content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
-        />
-        <meta property="twitter:image" content="/og-cover.png" />
-      </Head>
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://cocome.vercel.app/" />
+      <meta
+        property="twitter:title"
+        content="Hi Everyone — I am Diko Mahendra"
+      />
+      <meta
+        property="twitter:description"
+        content="web developer with ± 3 years of experience using NextJs, React Js and also React Native. interested in learning new things, used to working under pressure, working well with team, used to working remotely and ready to adjust skills according to company needs."
+      />
+      <meta property="twitter:image" content="/og-cover.png" />
 
       <body className="dark:bg-gray-800 min-h-screen relative">{children}</body>
     </html>
