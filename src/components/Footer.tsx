d07d520a8c94ddd0303a3a08f32eee16135727f9
DikoMahendraa <diko.dev99@gmail.com>
2024-03-24 21:42:58 +0700

    fix: typo i18n

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className="absolute bottom-10">
       <p className="text-gray-500 text-sm dark:text-gray-400">
-        &copy; {new Date().getFullYear()} Diko Mahendra. Made with ❤️ using
-        Next.js App Dir
+        &copy; {new Date().getFullYear()} Next. Made with ❤️ using Next.js App
+        Dir
       </p>
     </footer>
   )
