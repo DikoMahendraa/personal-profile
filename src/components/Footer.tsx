2b4e2b90361f25c0586f50aaa612e7aa1421e342
DikoMahendraa <diko.dev99@gmail.com>
2024-05-18 23:27:05 +0700

    hotfix: remove unused assets & layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="h-20 flex items-center justify-center">
+    <footer className="h-20 flex items-center justify-center absolute w-full bottom-0">
       <p className="text-gray-600 lg:text-sm text-xs dark:text-gray-400">
         &copy; {new Date().getFullYear()} Next. Made with ❤️ using Next.js App
         Dir
