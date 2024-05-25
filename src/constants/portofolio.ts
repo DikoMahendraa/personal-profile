f52395e56dd707e2850270b2bfdc18675d7143cd
DikoMahendraa <diko.dev99@gmail.com>
2024-05-25 15:19:40 +0700

    fix: adjust proper contain portofolio

--- src/constants/portofolio.ts
+++ src/constants/portofolio.ts
@@ -15,22 +15,17 @@ export const portofolio = {
       images: '/portofolio/personal/burgerapp.png',
       name: 'Burger City',
       todo: [
-        'Slicing UI using React Native, installing and running the React Native CLI application, running RN projects on real devices via usb cable and Wifi network. reusable and refactoring code.',
+        'Studied how React Native works.',
+        'Learned the differences between ReactJS and React Native, from setup to UI slicing.',
+        'Learned to implement clean code and refactor code.',
+        'Discussed with mentors regarding the usage of necessary packages and modules.',
+        'Ran React Native application using Wi-Fi and real device.',
       ],
       available: false,
       role: 'React Native Developer',
       tech: ['React Native'],
       link: '',
-      about: `<div>
-      <p>The application connects tenants, landlords, and professionals in the property rental process. It features several main functions:</p>
-      <ul><br />
-      <li><strong>Recons:</strong> This likely refers to a function related to "reconciliation" or organizing financial or transactional information for the parties involved.</li> <br />
-      <li><strong>Connect:</strong> This function facilitates the connection and interaction between tenants, landlords, and professionals involved in property rental.</li><br />
-      <li><strong>Manager (Landlord, Tenant, Professional):</strong> This feature serves as a management tool, enabling landlords, tenants, and professionals to oversee various aspects of the rental process.</li><br />
-      <li><strong>Profile:</strong> This section allows users to create and manage their personal profiles within the application.</li><br />
-      </ul>
-      <p>Each of these functions plays a crucial role in streamlining and enhancing the property rental experience by fostering communication, organization, and efficient management.</p>
-      </div>`,
+      about: `<p>is an application developed for learning and practicing React Native mobile app development. <br /> The goal of this application is to allow users to order food online without having to visit the restaurant. It features various functionalities such as a food menu, saving favorite items, and the checkout process. This application solely focuses on the UI and does not include integration.</p>`,
     },
     {
       layout_type: 'mobile',
@@ -39,46 +34,40 @@ export const portofolio = {
       images: '/portofolio/personal/doctor.png',
       name: 'My Doctor',
       todo: [
-        'Followed the course from 0, starting from RN project setup, slicing components. using atomic design pattern, integrating with firebase for authentication and for chat features.',
+        'Participated in online training from start to finish.',
+        'Initialized project using React Native.',
+        'Installed necessary dependencies such as Firebase and Redux.',
+        'Learned to utilize Firebase as a storage solution.',
+        'Utilized Firebase authentication feature for login and registration.',
+        'Implemented clean code and refactored code.',
+        'Utilized the atomic design pattern for project architecture.',
       ],
       available: false,
       role: 'React Native Developer',
       tech: ['React Native', 'Firebase'],
       link: '',
-      about: ` <div>
-      <p>The application connects tenants, landlords, and professionals in the property rental process. It features several main functions:</p>
-      <ul><br />
-      <li><strong>Recons:</strong> This likely refers to a function related to "reconciliation" or organizing financial or transactional information for the parties involved.</li> <br />
-      <li><strong>Connect:</strong> This function facilitates the connection and interaction between tenants, landlords, and professionals involved in property rental.</li><br />
-      <li><strong>Manager (Landlord, Tenant, Professional):</strong> This feature serves as a management tool, enabling landlords, tenants, and professionals to oversee various aspects of the rental process.</li><br />
-      <li><strong>Profile:</strong> This section allows users to create and manage their personal profiles within the application.</li><br />
-      </ul>
-      <p>Each of these functions plays a crucial role in streamlining and enhancing the property rental experience by fostering communication, organization, and efficient management.</p>
-      </div>`,
+      about: `<p>
+      is a chat application that acts as a bridge between patients and doctors, allowing for online consultation and medication ordering. <br /> It utilizes Firebase authentication and Firebase Firestore as a storage location for messages.
+      </p>`,
     },
     {
-      layout_type: 'desktop',
+      layout_type: 'mobile',
       type: 'personal',
       assets: [1, 2, 3, 4, 5, 6, 7, 8],
       images: '/portofolio/personal/financial.webp',
       name: 'Financial Planner',
       todo: [
-        'Determine the technology to be used, apply atomic design pattern as a folder structure. apply clean code and reusable components. integration with firebase.',
+        'Initialized project using Next.js.',
+        'Utilized atomic design pattern in Next.js app.',
+        'Integrated with Firebase for storage and authentication for login and registration.',
+        'Resolved bugs in display and non-functioning features.',
+        'Explored UI design ideas for an attractive appearance.',
       ],
       available: true,
       role: 'Frontend Developer',
       tech: ['NextJs', 'Typescript', 'Firebase', 'TailwindCSS'],
       link: 'https://financial-planner-ck.vercel.app/login',
-      about: ` <div>
-      <p>The application connects tenants, landlords, and professionals in the property rental process. It features several main functions:</p>
-      <ul><br />
-          <li><strong>Recons:</strong> This likely refers to a function related to "reconciliation" or organizing financial or transactional information for the parties involved.</li> <br />
-          <li><strong>Connect:</strong> This function facilitates the connection and interaction between tenants, landlords, and professionals involved in property rental.</li><br />
-          <li><strong>Manager (Landlord, Tenant, Professional):</strong> This feature serves as a management tool, enabling landlords, tenants, and professionals to oversee various aspects of the rental process.</li><br />
-          <li><strong>Profile:</strong> This section allows users to create and manage their personal profiles within the application.</li><br />
-      </ul>
-      <p>Each of these functions plays a crucial role in streamlining and enhancing the property rental experience by fostering communication, organization, and efficient management.</p>
-  </div>`,
+      about: `<p> is a web application that provides convenient features for managing expenses and income. It also includes a feature for calculating income to determine fund allocation according to needs such as emergency funds, savings, and spending money.</p>`,
     },
   ],
   company: [
@@ -239,12 +228,19 @@ export const portofolio = {
       <p>InsVault Insurance provides ease and convenience in managing your insurance, ensuring you get optimal protection without the hassle.</p>`,
     },
     {
-      layout_type: 'desktop',
+      layout_type: 'mobile',
       type: 'company',
       assets: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
       images: '/portofolio/company/hobids.webp',
       name: 'Hobids',
-      todo: [''],
+      todo: [
+        'Discussing with the client to determine the product/application flow to be developed.',
+        'Determining suitable technology according to the product type, and here I use Next.js and TypeScript because the application requires SEO.',
+        'Discussing with the design team, as well as the backend team regarding UI and API requirements.',
+        'Initializing the project using Next.js.',
+        'Installing dependencies and necessary packages such as Tailwind, Redux state management, etc.',
+        'Implementing clean code, refactoring, and bug fixing during development.',
+      ],
       role: 'Frontend Engineer',
       tech: [
         'ReactJs',
@@ -253,10 +249,17 @@ export const portofolio = {
         'TailwindCSS',
         'Ant Design',
         'Redux',
+        'PWA',
       ],
       link: '',
       available: false,
-      about: ``,
+      about: `<p><strong>Hobids: Your Unique E-commerce Experience</strong></p> <br />
+
+      <p><strong>Hobids</strong> is an innovative e-commerce application that offers a unique twist with its added feature of auctions. Built on a <strong>Progressive Web App (PWA)</strong> and web view, <strong>Hobids</strong> combines the convenience of traditional e-commerce platforms with the excitement of auctions.</p>
+      <br />
+      
+      <p>Unfortunately, the development of this promising application has been postponed due to client requests. However, <strong>Hobids</strong> remains poised to revolutionize the online shopping experience once development resumes.</p>
+      `,
     },
     {
       layout_type: 'desktop',
@@ -264,12 +267,29 @@ export const portofolio = {
       assets: [1, 2, 3, 4, 5],
       images: '/portofolio/company/zurich.webp',
       name: 'Dashboard Zurich Insurance',
-      todo: [''],
+      todo: [
+        'Studied Vue.js and Nuxt.js frameworks intensively for 7 days before starting development.',
+        'Initialized the project using Nuxt.js.',
+        'Installed necessary packages.',
+        'Integrated with API and Vuex state management.',
+        'Conducted meetings with the QA, PM, BE, and UI/UX teams.',
+        'Implemented analytics feature, handling data for status such as pending, rejected, verified, invalid, signed documents, and user management.',
+      ],
       role: 'Junior Frontend Developer',
       tech: ['VueJs', 'NuxtJs', 'Vuetify', 'Vuex'],
       link: '',
       available: false,
-      about: ``,
+      about: `<p>The Zurich Dashboard is an application for monitoring user-related insurance registration/loan processes. It includes several features such as analytics, checking the status of user loan applications (pending, invalid, rejected, or verified), monitoring activity logs such as user logins, etc., and user management for granting access to roles ranging from sysadmin, operational staff, manager, product manager, and several other roles.</p>
+      <br />
+      <p><strong>Key Features:</strong></p>
+      <br />
+      <ul class="list-disc">
+        <li class="mb-2">Analytics</li>
+        <li class="mb-2">Signed Documents</li>
+        <li class="mb-2">Acitvity Log</li>
+        <li>User Management</li>
+      </ul>
+      `,
     },
     {
       type: 'company',
@@ -278,11 +298,21 @@ export const portofolio = {
       images: '/portofolio/company/qatros.webp',
       name: 'Landing Page Qatros',
       role: 'Junior Frontend Developer',
-      todo: [''],
+      todo: [
+        'Studied Vue.js framework and Next.js intensively.',
+        'Developed applications for clients such as Zurich Dashboard and Artaboga (Parent Dashboard), adding several CRUD features to the Artaboga dashboard.',
+        'Conducted several Research and Development (R&D) sessions and produced products such as Auction and Liveness Detection.',
+        'Held sharing sessions regarding the usage of React vs. React Native.',
+        'Developed a new UI for internal landing page.',
+      ],
       tech: ['VueJs', 'Bootstrap'],
       link: 'https://qatros.com/',
       available: true,
-      about: ``,
+      about: `<p>Qatros is a technology startup offering a variety of service offerings for digital product needs (consultation, development, maintenance) and human resources needs (specific-house training, public training, bootcamp training). Currently, digital transformation is happening so massively, and changes have touched almost every sector of the industry. Qatros is ready to provide the best energy to solve various technology problems.</p>
+
+      <br />
+
+      <p>We build a culture of Technology From Rural People as an identity that we are proud of, and we are committed to providing products and services with the best quality. Where we live or where we come from is no longer a problem. It's all about working with a professional ethos, full of dedication, and a strong determination to keep learning forever.</p>`,
     },
     {
       layout_type: 'desktop',
@@ -291,11 +321,16 @@ export const portofolio = {
       images: '/portofolio/company/liveness.webp',
       name: 'Liveness Detection',
       role: 'Junior Frontend Developer',
-      todo: [''],
+      todo: [
+        'Initialized the project using Nuxt.js.',
+        'Integrated with a custom module to detect faces and expressions.',
+        'Implemented UI designs from Figma into code.',
+        'Implemented 3D assets for gestures using Three.js.',
+      ],
       tech: ['Bootstrap', 'NuxtJs', 'ThreeJs', 'Blender', 'Liveness Module'],
       link: '',
       available: false,
-      about: ``,
+      about: `Is an application used to validate based on gesture movements and facial expressions.`,
     },
   ],
 }
