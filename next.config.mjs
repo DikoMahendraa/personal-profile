9187d80c764a939dc90b16dc835431f1677b6f05
diko-mahendra <devteam@indesc.com>
2024-08-11 21:51:54 +0700

    feat: setup mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
   images: {
     remotePatterns: [
       {
@@ -12,6 +18,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'static-00.iconduck.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.assets.so',
       },
       {
         protocol: 'https',
@@ -37,4 +47,12 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
